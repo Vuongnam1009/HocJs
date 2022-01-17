9746262044712c18adda22ec0784fad115aa87b9
Nam <vuongnam1009@gmail.com>
2022-01-17 07:11:45 +0700

    update every2

--- every2.js
+++ every2.js
@@ -1,18 +1,18 @@
 Array.prototype.every2 = function(callback){
-    let i = 0
-     let result = true
-    for(;i< this.length;i++){
-     var test = callback(this[i],i)
-      if(!test){
-        result = false
-      }
-    }
-    return result
-  }
+  let result = true
+ for(var index in this){
+   if(this.hasOwnProperty(index))
+  var test = callback(this[index],index)
+   if(!test){
+     result = false
+   }
+ }
+ return result
+}
 
-  // eg.
-  var a = [1,'n',3,4]
-  var x = a.every2(function(b){
-    return typeof(b) === 'number'
-  })
-  console.log(x)
+// eg.
+var a = [1,2,3,4]
+var x = a.every2(function(b){
+ return typeof(b) === 'number'
+})
+console.log(x)
